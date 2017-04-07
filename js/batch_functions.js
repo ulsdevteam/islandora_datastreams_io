8a62f35a6e25522fd68c43dba0481d041fee51b0
Brian Gillingham <bgilling@pitt.edu>
2017-04-07 21:20:28 +0000

    ISLANDORA-137 ongoing development before release

--- js/batch_functions.js
+++ js/batch_functions.js
@@ -18,31 +18,32 @@
         $('#edit-pids-fetch-method-3').change(function() {
             hideall_but(3);
         });
-        hideall_but(0);
+        var i = $('input[name=pids_fetch_method]:checked', '#islandora-datastreams-io-export-form').val();
+        hideall_but(i);
     });
   
     function hideall_but(i) {
         if (i < 1) {
             $('.form-item-solr-query').show();
-            $('.form-item-batch-csv').hide();
+            $('.form-item-list-of-pids').hide();
             $('.form-item-collection').hide();
             $('.form-item-model').hide();
         }
         if (i == 1) {
             $('.form-item-solr-query').hide();
-            $('.form-item-batch-csv').show();
+            $('.form-item-list-of-pids').show();
             $('.form-item-collection').hide();
             $('.form-item-model').hide();
         }
         if (i == 2) {
             $('.form-item-solr-query').hide();
-            $('.form-item-batch-csv').hide();
+            $('.form-item-list-of-pids').hide();
             $('.form-item-collection').show();
             $('.form-item-model').hide();
         }
         if (i == 3) {
             $('.form-item-solr-query').hide();
-            $('.form-item-batch-csv').hide();
+            $('.form-item-list-of-pids').hide();
             $('.form-item-collection').hide();
             $('.form-item-model').show();
         }
